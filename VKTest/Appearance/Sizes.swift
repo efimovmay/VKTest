3c94127037a6735e525a01381638bda066074b61
Aleksey Efimov <efimov.may@yandex.ru>
2024-08-09 09:15:19 +0300

    Настройка проекта

---
+++ VKTest/Appearance/Sizes.swift
@@ -0,0 +1,33 @@
+//
+//  Sizes.swift
+//
+//  Created by Aleksey Efimov
+//
+
+import Foundation
+
+enum Sizes {
+	
+	// Common
+	static let cornerRadius: CGFloat = 10
+
+	enum LoginScreen {
+		static let screenHeigth: CGFloat = 500
+		static let doneButtonHeith: CGFloat = 50
+	}
+
+	enum Font {
+		static let title: CGFloat = 28
+		static let detailTitle: CGFloat = 22
+		static let regular: CGFloat = 16
+		static let titleEvent: CGFloat = 18
+	}
+	
+	enum Padding {
+		static let half: CGFloat = 8
+		static let normal: CGFloat = 16
+		static let semiDouble: CGFloat = 24
+		static let double: CGFloat = 32
+		static let max: CGFloat = 40
+	}
+}
---
+++ VKTest/Appearance/Strings.swift
@@ -0,0 +1,17 @@
+//
+//  Strings.swift
+//
+//  Created by Aleksey Efimov
+//
+
+import Foundation
+
+enum L10n {
+	
+	enum Common {
+		static let ok = NSLocalizedString("common.ok", comment: "")
+		static let error = NSLocalizedString("common.error", comment: "")
+		static let done = NSLocalizedString("common.done", comment: "")
+		static let cancel = NSLocalizedString("common.cancel", comment: "")
+	}
+}
