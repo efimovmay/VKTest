76930787f12da78a102ef6f26127c41a2acacb02
Aleksey Efimov <efimov.may@yandex.ru>
2024-08-09 09:03:13 +0300

    Initial Commit

---
+++ VKTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKTest
+//
+//  Created by Aleksey Efimov on 09.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
